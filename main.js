34082ced69afc0cf0d128f9feb3f2f079a55dcaf
sravankumar <128043902+devasanisravankumar@users.noreply.github.com>
2023-06-07 09:11:59 +0530

    First commit

---
+++ main.js
@@ -0,0 +1,10 @@
+// open the shopping no.of items
+function Myfunction(){
+    document.getElementById("display").style.display = "block";
+}
+   // close the shopping no.of items
+function closeitem(){
+    document.getElementById("display").style.display = "none";
+}
+
+
